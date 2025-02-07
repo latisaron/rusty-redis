6d15742fa98cfa7e527a7ee61dc0278f29726f1b
Aron <latis.aron@gmail.com>
2025-02-08 01:00:13 +0200

    functional tests of ary in ary and also remove the python script

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,6 @@ impl Serializer {
     }
 
     fn internal_deserialize(&self, incoming_str: &str, current_index: &mut usize) -> (Value, usize) {
-        println!("incoming istr is {:?}, current indx is {:?}", incoming_str, current_index);
         let basic_string = &incoming_str[*current_index..];
         let mut char_iter = basic_string.chars().enumerate();
         if let Some((_, first_char)) = char_iter.next() {
@@ -96,9 +95,6 @@ impl Serializer {
                     }
                 },
                 '*' => {
-                    println!("basic string is {:?}", basic_string);
-                    println!("basic strnig len is {:?}", basic_string.len());
-                    println!("current index is {:?}", current_index);
                     let mut split_iter = basic_string.split("\r\n");
                     let first_elem = split_iter.next().unwrap();
                     let number_of_elems = first_elem[1..].parse::<i32>().unwrap();
@@ -109,7 +105,7 @@ impl Serializer {
                         let mut tmp_vec = Vec::<Value>::new();
                         for _ in 0..number_of_elems {
                             *current_index = *current_index + first_elem.len() + 2;
-                            let result = self.internal_deserialize(basic_string, current_index);
+                            let result = self.internal_deserialize(incoming_str, current_index);
                             *current_index = *current_index + result.1;
                             tmp_vec.push(
                                 result.0
@@ -135,49 +131,49 @@ fn main() {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn null_value() {
-    //     let serializer = Serializer::new();
-    //     let string1 = String::from("$-1\r\n");
-    //     let value = serializer.deserialize(&string1);
-    //     assert_eq!(value, Value::Null);
-    //     assert_eq!(serializer.serialize(&value), string1);
-    // }
+    #[test]
+    fn null_value() {
+        let serializer = Serializer::new();
+        let string1 = String::from("$-1\r\n");
+        let value = serializer.deserialize(&string1);
+        assert_eq!(value, Value::Null);
+        assert_eq!(serializer.serialize(&value), string1);
+    }
 
-    // #[test]
-    // fn one_elem_ary() {
-    //     let serializer = Serializer::new();
-    //     let string2 = String::from("*1\r\n$4\r\nping\r\n");
-    //     let value = serializer.deserialize(&string2);
-    //     if let Value::Array(tmp_ary) = &value {
-    //         assert_eq!(tmp_ary[0], Value::Bulk(String::from("ping")));
-    //     }
-    //     assert_eq!(serializer.serialize(&value), string2);
-    // }
+    #[test]
+    fn one_elem_ary() {
+        let serializer = Serializer::new();
+        let string2 = String::from("*1\r\n$4\r\nping\r\n");
+        let value = serializer.deserialize(&string2);
+        if let Value::Array(tmp_ary) = &value {
+            assert_eq!(tmp_ary[0], Value::Bulk(String::from("ping")));
+        }
+        assert_eq!(serializer.serialize(&value), string2);
+    }
 
-    // #[test]
-    // fn echo_ary() {
-    //     let serializer = Serializer::new();
-    //     let string3 = String::from("*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n");
-    //     let value = serializer.deserialize(&string3);
-    //     if let Value::Array(tmp_ary) = &value {
-    //         assert_eq!(tmp_ary[0], Value::Bulk(String::from("echo")));
-    //         assert_eq!(tmp_ary[1], Value::Bulk(String::from("hello world")));
-    //     }
-    //     assert_eq!(serializer.serialize(&value), string3);
-    // }
+    #[test]
+    fn echo_ary() {
+        let serializer = Serializer::new();
+        let string3 = String::from("*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n");
+        let value = serializer.deserialize(&string3);
+        if let Value::Array(tmp_ary) = &value {
+            assert_eq!(tmp_ary[0], Value::Bulk(String::from("echo")));
+            assert_eq!(tmp_ary[1], Value::Bulk(String::from("hello world")));
+        }
+        assert_eq!(serializer.serialize(&value), string3);
+    }
 
-    // #[test]
-    // fn get_key_ary() {
-    //     let serializer = Serializer::new();
-    //     let string4 = String::from("*2\r\n$3\r\nget\r\n$3\r\nkey\r\n");
-    //     let value = serializer.deserialize(&string4);
-    //     if let Value::Array(tmp_ary) = &value {
-    //         assert_eq!(tmp_ary[0], Value::Bulk(String::from("get")));
-    //         assert_eq!(tmp_ary[1], Value::Bulk(String::from("key")));
-    //     }
-    //     assert_eq!(serializer.serialize(&value), string4);
-    // }
+    #[test]
+    fn get_key_ary() {
+        let serializer = Serializer::new();
+        let string4 = String::from("*2\r\n$3\r\nget\r\n$3\r\nkey\r\n");
+        let value = serializer.deserialize(&string4);
+        if let Value::Array(tmp_ary) = &value {
+            assert_eq!(tmp_ary[0], Value::Bulk(String::from("get")));
+            assert_eq!(tmp_ary[1], Value::Bulk(String::from("key")));
+        }
+        assert_eq!(serializer.serialize(&value), string4);
+    }
 
     #[test]
     fn ary_of_arys() {
@@ -189,45 +185,45 @@ mod tests {
             assert_eq!(tmp_ary[0], Value::Bulk(String::from("get")));
             assert_eq!(tmp_ary[1], Value::Bulk(String::from("key")));
             if let Value::Array(tmp_ary_2) = &tmp_ary[2] {
-                assert_eq!(tmp_ary_2[0], Value::Integer(3));
+                assert_eq!(tmp_ary_2[0], Value::String(String::from("3")));
             }
         }
-        // assert_eq!(serializer.serialize(&value), string4);
+        assert_eq!(serializer.serialize(&value), string4);
     }
 
-    // #[test]
-    // fn simple_string() {
-    //     let serializer = Serializer::new();
-    //     let string5 = String::from("+OK\r\n");
-    //     let value = Value::String(String::from("OK"));
-    //     assert_eq!(serializer.deserialize(&string5), value);
-    //     assert_eq!(serializer.serialize(&value), string5);
-    // }
+    #[test]
+    fn simple_string() {
+        let serializer = Serializer::new();
+        let string5 = String::from("+OK\r\n");
+        let value = Value::String(String::from("OK"));
+        assert_eq!(serializer.deserialize(&string5), value);
+        assert_eq!(serializer.serialize(&value), string5);
+    }
 
-    // #[test]
-    // fn error() {
-    //     let serializer = Serializer::new();
-    //     let string6 = String::from("-Error message\r\n");
-    //     let value = Value::Error(String::from("Error message"));
-    //     assert_eq!(serializer.deserialize(&string6), value);
-    //     assert_eq!(serializer.serialize(&value), string6);
-    // }
+    #[test]
+    fn error() {
+        let serializer = Serializer::new();
+        let string6 = String::from("-Error message\r\n");
+        let value = Value::Error(String::from("Error message"));
+        assert_eq!(serializer.deserialize(&string6), value);
+        assert_eq!(serializer.serialize(&value), string6);
+    }
 
-    // #[test]
-    // fn empty_bulk() {
-    //     let serializer = Serializer::new();
-    //     let string7 = String::from("$0\r\n\r\n");
-    //     let value = Value::Bulk(String::from(""));
-    //     assert_eq!(serializer.deserialize(&string7), value);
-    //     assert_eq!(serializer.serialize(&value), string7);
-    // }
+    #[test]
+    fn empty_bulk() {
+        let serializer = Serializer::new();
+        let string7 = String::from("$0\r\n\r\n");
+        let value = Value::Bulk(String::from(""));
+        assert_eq!(serializer.deserialize(&string7), value);
+        assert_eq!(serializer.serialize(&value), string7);
+    }
 
-    // #[test]
-    // fn simple_string_again() {
-    //     let serializer = Serializer::new();
-    //     let string8 = String::from("+hello world\r\n");
-    //     let value = Value::String(String::from("hello world"));
-    //     assert_eq!(serializer.deserialize(&string8), value);
-    //     assert_eq!(serializer.serialize(&value), string8);
-    // }
+    #[test]
+    fn simple_string_again() {
+        let serializer = Serializer::new();
+        let string8 = String::from("+hello world\r\n");
+        let value = Value::String(String::from("hello world"));
+        assert_eq!(serializer.deserialize(&string8), value);
+        assert_eq!(serializer.serialize(&value), string8);
+    }
 }
